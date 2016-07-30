2e60ac8bf2b4a8165599d690eaa0f1fd18adb7ad
Drew Keller <drew@eightycolumns.com>
2016-07-29 21:25:06 -0400

    Check that augend isn't NULL

--- test/null-pointer-error.c
+++ test/null-pointer-error.c
@@ -6,6 +6,7 @@
 
 #include "src/romcalc.h"
 
+static char sum[ROMCALC_MAX_LENGTH];
 static char augend[ROMCALC_MAX_LENGTH];
 static char addend[ROMCALC_MAX_LENGTH];
 
@@ -15,10 +16,17 @@ START_TEST(sum_cannot_be_NULL) {
   ck_assert_int_eq(expected, actual);
 } END_TEST
 
+START_TEST(augend_cannot_be_NULL) {
+  int expected = ROMCALC_NULL_POINTER_ERROR;
+  int actual = add(sum, NULL, addend);
+  ck_assert_int_eq(expected, actual);
+} END_TEST
+
 TCase *null_pointer_error(void) {
   TCase *null_pointer_error = tcase_create("Null Pointer Error");
 
   tcase_add_test(null_pointer_error, sum_cannot_be_NULL);
+  tcase_add_test(null_pointer_error, augend_cannot_be_NULL);
 
   return null_pointer_error;
 }
