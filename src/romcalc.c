2ace902a32e3cb1396d2e2bece5bb4021c2a07bc
Drew Keller <drew@eightycolumns.com>
2016-07-29 22:04:50 -0400

    Accept I as a valid operand

--- src/romcalc.c
+++ src/romcalc.c
@@ -1,10 +1,19 @@
 #include "src/romcalc.h"
 
+#include <assert.h>
+#include <regex.h>
+#include <stdbool.h>
 #include <stddef.h>
+
+static bool is_roman_numeral(const char *string);
 
 int add(char *sum, const char *augend, const char *addend) {
   if (sum == NULL || augend == NULL || addend == NULL) {
     return ROMCALC_NULL_POINTER_ERROR;
+  }
+
+  if (!is_roman_numeral(augend) || !is_roman_numeral(addend)) {
+    return ROMCALC_INVALID_OPERAND_ERROR;
   }
 
   return ROMCALC_SUCCESS;
@@ -15,5 +24,29 @@ int subtract(char *difference, const char *minuend, const char *subtrahend) {
     return ROMCALC_NULL_POINTER_ERROR;
   }
 
+  if (!is_roman_numeral(minuend) || !is_roman_numeral(subtrahend)) {
+    return ROMCALC_INVALID_OPERAND_ERROR;
+  }
+
   return ROMCALC_SUCCESS;
+}
+
+static bool is_roman_numeral(const char *string) {
+  assert(string != NULL);
+
+  regex_t regex;
+
+  char pattern[] = "^I$";
+
+  int flags = REG_EXTENDED|REG_NOSUB;
+
+  if (regcomp(&regex, pattern, flags) != 0) {
+    return false;
+  }
+
+  bool result = (regexec(&regex, string, 0, NULL, 0) == 0);
+
+  regfree(&regex);
+
+  return result;
 }
--- test/error-handling.c
+++ test/error-handling.c
@@ -2,11 +2,13 @@
 
 #include <check.h>
 
+#include "test/success.h"
 #include "test/null-pointer-error.h"
 
 Suite *error_handling(void) {
   Suite *error_handling = suite_create("Error Handling");
 
+  suite_add_tcase(error_handling, success());
   suite_add_tcase(error_handling, null_pointer_error());
 
   return error_handling;
