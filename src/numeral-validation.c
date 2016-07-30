9c317642faf2d33836aae692c09e105a361a3dbc
Drew Keller <drew@eightycolumns.com>
2016-07-30 17:43:50 -0400

    Extract numeral-conversion.{c,h} from romcalc.c

---
+++ src/numeral-validation.c
@@ -0,0 +1,83 @@
+#include "src/numeral-validation.h"
+
+#include <assert.h>
+#include <ctype.h>
+#include <regex.h>
+#include <stdbool.h>
+#include <string.h>
+
+static bool is_empty_string(const char *string);
+static bool is_mixed_case(const char *string);
+static bool includes_lowercase(const char *string);
+static bool includes_uppercase(const char *string);
+
+bool is_roman_numeral(const char *string) {
+  assert(string != NULL);
+
+  if (is_empty_string(string)) {
+    return false;
+  }
+
+  if (is_mixed_case(string)) {
+    return false;
+  }
+
+  regex_t regex;
+
+  char pattern[] =
+    "^M{0,3}"           // Thousands
+    "(D?C{0,3}|C[DM])"  // Hundreds
+    "(L?X{0,3}|X[LC])"  // Tens
+    "(V?I{0,3}|I[VX])$" // Ones
+  ;
+
+  int flags = REG_EXTENDED|REG_ICASE|REG_NOSUB;
+
+  if (regcomp(&regex, pattern, flags) != 0) {
+    return false;
+  }
+
+  bool result = (regexec(&regex, string, 0, NULL, 0) == 0);
+
+  regfree(&regex);
+
+  return result;
+}
+
+static bool is_empty_string(const char *string) {
+  assert(string != NULL);
+  return strcmp("", string) == 0;
+}
+
+static bool is_mixed_case(const char *string) {
+  assert(string != NULL);
+  return includes_lowercase(string) && includes_uppercase(string);
+}
+
+static bool includes_lowercase(const char *string) {
+  assert(string != NULL);
+
+  size_t string_length = strlen(string);
+
+  for (size_t i = 0; i < string_length; i += 1) {
+    if (islower(string[i])) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+static bool includes_uppercase(const char *string) {
+  assert(string != NULL);
+
+  size_t string_length = strlen(string);
+
+  for (size_t i = 0; i < string_length; i += 1) {
+    if (isupper(string[i])) {
+      return true;
+    }
+  }
+
+  return false;
+}
--- src/romcalc.c
+++ src/romcalc.c
@@ -1,16 +1,8 @@
 #include "src/romcalc.h"
 
-#include <assert.h>
-#include <ctype.h>
-#include <regex.h>
-#include <stdbool.h>
-#include <string.h>
+#include <stddef.h>
 
-static bool is_roman_numeral(const char *string);
-static bool is_empty_string(const char *string);
-static bool is_mixed_case(const char *string);
-static bool includes_lowercase(const char *string);
-static bool includes_uppercase(const char *string);
+#include "src/numeral-validation.h"
 
 int add(char *sum, const char *augend, const char *addend) {
   if (sum == NULL || augend == NULL || addend == NULL) {
@@ -34,75 +26,4 @@ int subtract(char *difference, const char *minuend, const char *subtrahend) {
   }
 
   return ROMCALC_SUCCESS;
-}
-
-static bool is_roman_numeral(const char *string) {
-  assert(string != NULL);
-
-  if (is_empty_string(string)) {
-    return false;
-  }
-
-  if (is_mixed_case(string)) {
-    return false;
-  }
-
-  regex_t regex;
-
-  char pattern[] =
-    "^M{0,3}"           // Thousands
-    "(D?C{0,3}|C[DM])"  // Hundreds
-    "(L?X{0,3}|X[LC])"  // Tens
-    "(V?I{0,3}|I[VX])$" // Ones
-  ;
-
-  int flags = REG_EXTENDED|REG_ICASE|REG_NOSUB;
-
-  if (regcomp(&regex, pattern, flags) != 0) {
-    return false;
-  }
-
-  bool result = (regexec(&regex, string, 0, NULL, 0) == 0);
-
-  regfree(&regex);
-
-  return result;
-}
-
-static bool is_empty_string(const char *string) {
-  assert(string != NULL);
-  return strcmp("", string) == 0;
-}
-
-static bool is_mixed_case(const char *string) {
-  assert(string != NULL);
-  return includes_lowercase(string) && includes_uppercase(string);
-}
-
-static bool includes_lowercase(const char *string) {
-  assert(string != NULL);
-
-  size_t string_length = strlen(string);
-
-  for (size_t i = 0; i < string_length; i += 1) {
-    if (islower(string[i])) {
-      return true;
-    }
-  }
-
-  return false;
-}
-
-static bool includes_uppercase(const char *string) {
-  assert(string != NULL);
-
-  size_t string_length = strlen(string);
-
-  for (size_t i = 0; i < string_length; i += 1) {
-    if (isupper(string[i])) {
-      return true;
-    }
-  }
-
-  return false;
 }
