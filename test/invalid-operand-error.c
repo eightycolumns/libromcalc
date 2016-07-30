d8d870af4451ea31df054840ab891028386bf16e
Drew Keller <drew@eightycolumns.com>
2016-07-30 06:51:55 -0400

    Reject VV as an invalid operand

--- test/invalid-operand-error.c
+++ test/invalid-operand-error.c
@@ -14,10 +14,17 @@ START_TEST(IIII_is_an_invalid_operand) {
   ck_assert_int_eq(expected, actual);
 } END_TEST
 
+START_TEST(VV_is_an_invalid_operand) {
+  int expected = ROMCALC_INVALID_OPERAND_ERROR;
+  int actual = add(sum, "VV", "I");
+  ck_assert_int_eq(expected, actual);
+} END_TEST
+
 TCase *invalid_operand_error(void) {
   TCase *invalid_operand_error = tcase_create("Invalid Operand Error");
 
   tcase_add_test(invalid_operand_error, IIII_is_an_invalid_operand);
+  tcase_add_test(invalid_operand_error, VV_is_an_invalid_operand);
 
   return invalid_operand_error;
 }
