04b05f324202420384db9b8240c5ca2672cbba7b
Drew Keller <drew@eightycolumns.com>
2016-07-30 13:12:43 -0400

    Accept MMMDCCCLXXXVIII as a valid operand

--- test/success.c
+++ test/success.c
@@ -110,6 +110,12 @@ START_TEST(MMM_is_a_valid_operand) {
   ck_assert_int_eq(expected, actual);
 } END_TEST
 
+START_TEST(MMMDCCCLXXXVIII_is_a_valid_operand) {
+  int expected = ROMCALC_SUCCESS;
+  int actual = add(sum, "MMMDCCCLXXXVIII", "I");
+  ck_assert_int_eq(expected, actual);
+} END_TEST
+
 TCase *success(void) {
   TCase *success = tcase_create("Success");
 
@@ -133,6 +139,8 @@ TCase *success(void) {
 
   tcase_add_test(success, M_is_a_valid_operand);
   tcase_add_test(success, MMM_is_a_valid_operand);
+
+  tcase_add_test(success, MMMDCCCLXXXVIII_is_a_valid_operand);
 
   return success;
 }
