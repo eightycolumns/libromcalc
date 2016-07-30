39c1caef79a2c8bab1541d936145941f69b6681c
Drew Keller <drew@eightycolumns.com>
2016-07-30 13:15:23 -0400

    Accept MMMCMXCIX as a valid operand

--- test/success.c
+++ test/success.c
@@ -7,6 +7,7 @@
 #include "src/romcalc.h"
 
 static char sum[ROMCALC_MAX_LENGTH];
+static char difference[ROMCALC_MAX_LENGTH];
 
 START_TEST(I_is_a_valid_operand) {
   int expected = ROMCALC_SUCCESS;
@@ -116,6 +117,12 @@ START_TEST(MMMDCCCLXXXVIII_is_a_valid_operand) {
   ck_assert_int_eq(expected, actual);
 } END_TEST
 
+START_TEST(MMMCMXCIX_is_a_valid_operand) {
+  int expected = ROMCALC_SUCCESS;
+  int actual = subtract(difference, "MMMCMXCIX", "I");
+  ck_assert_int_eq(expected, actual);
+} END_TEST
+
 TCase *success(void) {
   TCase *success = tcase_create("Success");
 
@@ -141,6 +148,8 @@ TCase *success(void) {
   tcase_add_test(success, MMM_is_a_valid_operand);
 
   tcase_add_test(success, MMMDCCCLXXXVIII_is_a_valid_operand);
+
+  tcase_add_test(success, MMMCMXCIX_is_a_valid_operand);
 
   return success;
 }
