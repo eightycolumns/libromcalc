37af7df0823384564a134141a26da7081f3e0d24
Drew Keller <drew@eightycolumns.com>
2016-07-30 19:32:26 -0400

    Test V + IV = IX

--- src/romcalc.c
+++ src/romcalc.c
@@ -12,6 +12,7 @@ typedef struct {
 } Numeral;
 
 static Numeral numerals[] = {
+  {"IX", 9},
   {"V", 5},
   {"IV", 4},
   {"I", 1}
--- test/addition.c
+++ test/addition.c
@@ -26,12 +26,19 @@ START_TEST(IV_plus_I_is_V) {
   ck_assert_str_eq(expected, actual);
 } END_TEST
 
+START_TEST(V_plus_IV_is_IX) {
+  char expected[] = "IX";
+  add(actual, "V", "IV");
+  ck_assert_str_eq(expected, actual);
+} END_TEST
+
 TCase *addition(void) {
   TCase *addition = tcase_create("Addition");
 
   tcase_add_test(addition, I_plus_I_is_II);
   tcase_add_test(addition, II_plus_II_is_IV);
   tcase_add_test(addition, IV_plus_I_is_V);
+  tcase_add_test(addition, V_plus_IV_is_IX);
 
   return addition;
 }
