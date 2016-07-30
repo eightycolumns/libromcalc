353dda8c77774bcab8546a376da224a672d71d1d
Drew Keller <drew@eightycolumns.com>
2016-07-30 12:57:08 -0400

    Accept CM as a valid operand

--- test/success.c
+++ test/success.c
@@ -92,6 +92,12 @@ START_TEST(D_is_a_valid_operand) {
   ck_assert_int_eq(expected, actual);
 } END_TEST
 
+START_TEST(CM_is_a_valid_operand) {
+  int expected = ROMCALC_SUCCESS;
+  int actual = add(sum, "CM", "I");
+  ck_assert_int_eq(expected, actual);
+} END_TEST
+
 TCase *success(void) {
   TCase *success = tcase_create("Success");
 
@@ -111,6 +117,7 @@ TCase *success(void) {
   tcase_add_test(success, CCC_is_a_valid_operand);
   tcase_add_test(success, CD_is_a_valid_operand);
   tcase_add_test(success, D_is_a_valid_operand);
+  tcase_add_test(success, CM_is_a_valid_operand);
 
   return success;
 }
