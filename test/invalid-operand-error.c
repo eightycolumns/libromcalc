0f879a427c5d100b763d2a810763bd289d66cda3
Drew Keller <drew@eightycolumns.com>
2016-07-30 13:05:27 -0400

    Reject DM as an invalid operand

--- test/invalid-operand-error.c
+++ test/invalid-operand-error.c
@@ -74,6 +74,12 @@ START_TEST(CCD_is_an_invalid_operand) {
   ck_assert_int_eq(expected, actual);
 } END_TEST
 
+START_TEST(DM_is_an_invalid_operand) {
+  int expected = ROMCALC_INVALID_OPERAND_ERROR;
+  int actual = add(sum, "DM", "I");
+  ck_assert_int_eq(expected, actual);
+} END_TEST
+
 TCase *invalid_operand_error(void) {
   TCase *invalid_operand_error = tcase_create("Invalid Operand Error");
 
@@ -90,6 +96,7 @@ TCase *invalid_operand_error(void) {
   tcase_add_test(invalid_operand_error, CCCC_is_an_invalid_operand);
   tcase_add_test(invalid_operand_error, DD_is_an_invalid_operand);
   tcase_add_test(invalid_operand_error, CCD_is_an_invalid_operand);
+  tcase_add_test(invalid_operand_error, DM_is_an_invalid_operand);
 
   return invalid_operand_error;
 }
