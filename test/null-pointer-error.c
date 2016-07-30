9810500d41bf57c03c5c55910c34e52c35b6ecad
Drew Keller <drew@eightycolumns.com>
2016-07-29 21:31:55 -0400

    Check that difference isn't NULL

--- test/null-pointer-error.c
+++ test/null-pointer-error.c
@@ -10,6 +10,9 @@ static char sum[ROMCALC_MAX_LENGTH];
 static char augend[ROMCALC_MAX_LENGTH];
 static char addend[ROMCALC_MAX_LENGTH];
 
+static char minuend[ROMCALC_MAX_LENGTH];
+static char subtrahend[ROMCALC_MAX_LENGTH];
+
 START_TEST(sum_cannot_be_NULL) {
   int expected = ROMCALC_NULL_POINTER_ERROR;
   int actual = add(NULL, augend, addend);
@@ -28,12 +31,20 @@ START_TEST(addend_cannot_be_NULL) {
   ck_assert_int_eq(expected, actual);
 } END_TEST
 
+START_TEST(difference_cannot_be_NULL) {
+  int expected = ROMCALC_NULL_POINTER_ERROR;
+  int actual = subtract(NULL, minuend, subtrahend);
+  ck_assert_int_eq(expected, actual);
+} END_TEST
+
 TCase *null_pointer_error(void) {
   TCase *null_pointer_error = tcase_create("Null Pointer Error");
 
   tcase_add_test(null_pointer_error, sum_cannot_be_NULL);
   tcase_add_test(null_pointer_error, augend_cannot_be_NULL);
   tcase_add_test(null_pointer_error, addend_cannot_be_NULL);
+
+  tcase_add_test(null_pointer_error, difference_cannot_be_NULL);
 
   return null_pointer_error;
 }
