ed67c2069501a7bd0a226a546d37e0ebd0b29fbc
Drew Keller <drew@eightycolumns.com>
2016-07-30 21:46:22 -0400

    Test MM - M = M

--- test/subtraction.c
+++ test/subtraction.c
@@ -20,11 +20,18 @@ START_TEST(MMMDCCCLXXXVIII_minus_MDCCCLXXXVIII_is_MM) {
   ck_assert_str_eq(expected, actual);
 } END_TEST
 
+START_TEST(MM_minus_M_is_M) {
+  char expected[] = "M";
+  subtract(actual, "MM", "M");
+  ck_assert_str_eq(expected, actual);
+} END_TEST
+
 TCase *subtraction(void) {
   TCase *subtraction = tcase_create("Subtraction");
 
   tcase_add_test(subtraction, MMMCMXCIX_minus_CXI_is_MMMDCCCLXXXVIII);
   tcase_add_test(subtraction, MMMDCCCLXXXVIII_minus_MDCCCLXXXVIII_is_MM);
+  tcase_add_test(subtraction, MM_minus_M_is_M);
 
   return subtraction;
 }
